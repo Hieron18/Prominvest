8a43f24e5247a5b58bf0e57a23bbb58a49ca5e6e
egor <egor2323233232327@gmail.com>
2021-01-25 15:46:13 +0300

    job__time

--- Prominvest/js/script.js
+++ Prominvest/js/script.js
@@ -77,3 +77,19 @@ var swiper = new Swiper('.swiper-containerperson_479', {
     $('.header__burger, body, .sidebar__content').removeClass('burger');
   })
 })
+
+var countDownDate = new Date("Feb 27, 2021 00:00:00").getTime();
+var countDownFunction = setInterval(function (){
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000*60*60*24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("time").innerHTML =
+    days + " дня " + hours + ":" + minutes + ":" + seconds;
+    if (distance < 0){
+      clearInterval(countDownFunction);
+      document.getElementById("time").innerHTML = "Время истекло"
+    }
+}, 1000)
