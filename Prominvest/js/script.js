f1b79bce615b65a1b98b993686dd37584af61d43
egor <egor2323233232327@gmail.com>
2021-01-25 15:18:05 +0300

    job__css_1

--- Prominvest/js/script.js
+++ Prominvest/js/script.js
@@ -63,19 +63,6 @@ var swiper = new Swiper('.swiper-containerperson_479', {
    },
  });
  $(document).ready(function() {
-  const anchors = document.querySelectorAll('a[href*="#move"]');
-
-  for (let anchor of anchors){
-    anchor.addEventListener("click", function(event) {
-      event.preventDefault();
-      const blockID = anchor.getAttribute('href')
-      document.querySelector('' + blockID).scrollIntoView({
-        behavior: "smooth",
-        block: "start"
-      })
-    })
-    console.log(anchors)
-  }
     $('.header__burger').click(function(){
       $('.header__burger, body, .sidebar__content').toggleClass('burger');
     })
@@ -90,45 +77,3 @@ var swiper = new Swiper('.swiper-containerperson_479', {
     $('.header__burger, body, .sidebar__content').removeClass('burger');
   })
 })
-//  $(document).ready(function() {
-//    $(window).resize(function() {
-//       if ($(window).width() < '1024'){
-//          var swiper = new Swiper('.swiper-container', {
-//             slidesPerView: 3,
-//             spaceBetween: 10,
-//             navigation: {
-//               nextEl: '.swiper-button-next',
-//               prevEl: '.swiper-button-prev',
-//             },
-//           });
-//   }
-//   if ($(window).width() < '768'){
-//    var swiper = new Swiper('.swiper-container', {
-//       slidesPerView: 2,
-//       spaceBetween: 10,
-//       navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//       },
-//     });
-//    }
-//    if ($(window).width() < '479'){
-//       var swiper = new Swiper('.swiper-container', {
-//          slidesPerView: 1.3,
-//          spaceBetween: 20,
-//          navigation: {
-//            nextEl: '.swiper-button-next',
-//            prevEl: '.swiper-button-prev',
-//          },
-//        });
-//        var swiper_2 = new Swiper('.swiper-container_2', {
-//          slidesPerView: 1.3,
-//          spaceBetween: 20,
-//          navigation: {
-//            nextEl: '.swiper-button-next_2',
-//            prevEl: '.swiper-button-prev_2',
-//          },
-//        });
-//       }
-// });
-// });
