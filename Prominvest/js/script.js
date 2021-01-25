148325c0d0af44fdc4e3c6ea93654239968736c9
egor <egor2323233232327@gmail.com>
2021-01-25 14:36:53 +0300

    job__text

--- Prominvest/js/script.js
+++ Prominvest/js/script.js
@@ -63,6 +63,18 @@ var swiper = new Swiper('.swiper-containerperson_479', {
    },
  });
  $(document).ready(function() {
+  const anchors = document.querySelectorAll('a[href*="#move"]');
+
+  for (let anchor of anchors){
+    anchor.addEventListener("click", function(event) {
+      event.preventDefault();
+      const blockID = anchor.getAttribute('href')
+      document.querySelector('' + blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      })
+    })
+  }
     $('.header__burger').click(function(){
       $('.header__burger, body, .sidebar__content').toggleClass('burger');
     })
@@ -119,16 +131,3 @@ var swiper = new Swiper('.swiper-containerperson_479', {
 //       }
 // });
 // });
-
-// const anchors = document.querySelectorAll('a[href*="#move"]');
-
-// for (let anchor of anchors){
-//   anchor.addEventListener("click", function(event) {
-//     event.preventDefault();
-//     const blockID = anchor.getAttribute('href')
-//     document.querySelector('' + blockID).scrollIntoView({
-//       behavior: "smooth",
-//       block: "start"
-//     })
-//   })
-// }
